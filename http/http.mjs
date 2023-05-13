d727a812b9f76bdf3f69f759d5c061100f46f029
LE <larrychang@outlook.com>
2023-05-12 20:58:11 -0700

    xx

---
+++ http/http.mjs
@@ -0,0 +1,58 @@
+import mongodb from 'mongodb'
+// Using express for rest API
+import express from 'express'
+import bodyParser from 'body-parser'
+import path from 'path'
+
+const app = express()
+const port = 8080
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+var counter = 0
+
+/**
+ * This function does the follwing things:
+ * 1. Connect to mongodb.
+ * 2. For insert/ API, insert into movies.
+ * 2. For list/ API, list all of the movies under a author.
+ */
+async function main() {
+    const uri = "mongodb://localhost:27017"
+    const client = new mongodb.MongoClient(uri)
+    client.connect()
+    console.log("in db connection...")
+    const collection = client.db('mydatabase').collection('movies');
+    app.set('view engine', 'ejs');
+
+    app.get('/list/:author', async function (req, res) {
+        console.log("in list request..." + req.params.author)
+        var movies = []
+        const query = { author: req.params.author }
+        const array = collection.find(query)
+        for await (const doc of array) {
+            // list of documents
+            console.log(doc)
+            movies.push({ time: doc.time, name: doc.name, author: doc.author })
+        }
+        res.render(path.resolve("html/list"), { movies: JSON.stringify(movies) })
+    })
+
+    // This is anti-pattern....
+    app.get('/insert', async function (req, res) {
+        console.log("in insert get request...")
+        res.render(path.resolve("html/insert"), { result: "" })
+    })
+
+    app.post('/insert', async function (req, res) {
+        console.log("in insert post request...")
+        let date = new Date().toJSON();
+        const { name, author } = req.body
+        const query = { time: date, name: name, author: author }
+        await collection.insertOne(query)
+        res.render(path.resolve("html/insert"), { result: JSON.stringify(query) })
+    })
+
+    app.listen(port, () => console.log(`starting to listen at ${port}...`))
+}
+
+main()
--- web/socket.mjs
+++ web/socket.mjs
@@ -1,6 +1,6 @@
 import http from 'http';
 import ws from 'websocket';
-import redis from 'redis';
+import redis from 'ioredis';
 
 const APPID = process.env.APPID
 const WebSocketServer = ws.server
@@ -10,27 +10,80 @@ const websocket = new WebSocketServer({
 })
 const sockets = []
 
-const subsriber = redis.createClient('redis://redis:6379')
-const publisher = redis.createClient('redis://redis:6379')
+/*
+let options = {socket_keepalive : true, socket_initialdelay : 200000};
+const subsriber = redis.createClient({
+    host: "redis",
+    port: 6379,
+    options: options,
+    pingInterval: 1000 // <- add pingInterval
+})
+const publisher = redis.createClient({
+    host: "redis",
+    options: options,
+    port: 6379,
+    pingInterval: 1000 // <- add pingInterval
+})
 
-subsriber.on("message", function(channel, message) {
-    console.log(`received: ${message}`)
-    sockets.forEach(c => c.send(message))
+subsriber.on('connect', function () {
+    console.log("connecting subscriber")
+    var socket = subsriber.stream
+    socket.setKeepAlive(true, 30 * 1000)
+})
+publisher.on('connect', function () {
+    console.log("connecting publisher")
+    var socket = publisher.stream
+    socket.setKeepAlive(true, 30 * 1000)
+})
+*/
+/*
+const subsriber = redis.createClient('redis://rds:6379')
+const publisher = redis.createClient('redis://rds:6379')
+*/
+const subsriber = new redis({
+    host: 'rds',
+    connectTimeout: 10000,
+    // lazyConnect: true,
+    port: 6379
+})
+const publisher = new redis({
+    host: 'rds',
+    connectTimeout: 10000,
+    // lazyConnect: true,
+    port: 6379
+})
+subsriber.on('error', err => console.log("Redis server error:" + err))
+
+subsriber.on("message", function (channel, message) {
+    try {
+        console.log(`subscriber received: ${message}`)
+        sockets.forEach(c => c.send(message))
+    }
+    catch (ex) {
+        console.log("ERRO: " + ex)
+    }
 })
 
 subsriber.subscribe("chat")
 
 httpserver.listen(8080, () => console.log("My server is listening on 8080."))
-websocket.on("request", request=> {
+websocket.on("request", request => {
     const con = request.accept(null, request.origin)
     con.on("open", () => console.log("opened"))
     con.on("close", () => console.log("closed"))
     con.on("message", message => {
-        console.log("received message: " + message.utf8Data)
+        try {
+        console.log(publisher.set('foo', 'bar')); // 'OK'
+        console.log("publish message: " + message.utf8Data)
         // con.send(message.utf8Data);
-        publisher.publish("chat", message.utf8Data)
+        console.log(publisher.publish("chat", message.utf8Data))
+        console.log("published message")
+        }
+        catch (ex) {
+            console.log("ERROR: " + ex)
+        }
     })
 
-    setTimeout(() => con.send('connected successfully.'), 5000)
+    setTimeout(() => con.send('connected successfully.'), 50000)
     sockets.push(con)
 })
